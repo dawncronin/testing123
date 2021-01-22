ce41e59c99748cf457660d3b20035119d3a474e4
Dawn Cronin <dawncronin7@gmail.com>
2021-01-22 11:26:17 -0800

    redirect signin to home page when logged in, added user action types to ensure consistency

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import { setCurrentUser } from './redux/user/user.actions'
 
 import { connect } from 'react-redux'
-import { Route , Switch} from 'react-router-dom'
+import { Route , Switch, Redirect} from 'react-router-dom'
 import HomePage from './pages/homepage.component'
 import ShopPage from './pages/shoppage.component'
 import Header from './components/header.component'
@@ -41,16 +41,27 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
+        <Route 
+          exact path='/signin' 
+          render={() => 
+            this.props.currentUser ? (
+            <Redirect to='/' />
+            ) : (
+            <SignInAndSignUp />
+            )
+          }
+        />
       </Switch>
       
     </div>
   )}
 }
-
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
